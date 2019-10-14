c441f875b5341047176818199096384b3d30363a
Felix Montreuil <felixmontreuil@gmail.com>
2019-10-14 10:40:30 -0400

    reduit le nombre de model de la db

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import json
 import datetime
 
 import click
-from flask import Flask, jsonify, request, abort, redirect, url_for
+from flask import Flask, jsonify, request, abort, redirect, url_for, Response
 import peewee as p
 from playhouse.shortcuts import model_to_dict, dict_to_model
-from playhouse.sqlite_ext import *
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 
@@ -38,36 +37,16 @@ class Product(BaseModel):
     name = p.TextField()
     weight = p.IntegerField()
 
-class ShippingInformation(BaseModel):
-    id=p.AutoField(primary_key=True)
-    country=p.TextField()
-    address=p.TextField()
-    postalCode=p.TextField()
-    city=p.TextField()
-    province=p.TextField()
-
-class Transaction(BaseModel):
-    id=p.AutoField(primary_key=True)
-    success=p.BooleanField()
-    amount_charged=p.DoubleField()
-
-class CreditCard(BaseModel):
-    id=p.AutoField(primary_key=True)
-    name=p.TextField()
-    number=p.TextField()
-    exipration_year=p.IntegerField()
-    cvv=p.IntegerField()
-    expiration_month=p.IntegerField()
-
 class Order(BaseModel):
     id=p.AutoField(primary_key=True)
     product = JSONField(p.TextField(null=True))
-    creditCard=p.ForeignKeyField(CreditCard, backref="creditcard", null=True)
-    shippingInformation=p.ForeignKeyField(ShippingInformation, backref="info shiping", null=True)
-    transaction=p.ForeignKeyField(Transaction, backref="transactions", null=True)
-    shippingPrice=p.DoubleField(null=True)
+    creditCard=JSONField(default={})
+    shippingInformation=JSONField(default={})
+    transaction= JSONField(default={})
+    shippingPrice=p.DoubleField(default=0)
     email=p.TextField(null=True)
-    paid=p.BooleanField(null=True)
+    paid=p.BooleanField(default=False)
+    total_price=p.DoubleField(default=0)
 
 def perform_request(uri, method="GET", data=None):
     request = Request('https://caissy.dev/shops/{0}'.format(uri))
@@ -115,14 +94,20 @@ def products_get():
 
 @app.route('/order', methods=['POST'])
 def order_post():
+	json_payload = []
 	if not request.is_json:
 		return abort(400)
+	try:
+		json_payload = request.json['productz']
+	except KeyError:
+		return jsonify({"code": "missing-fields", "name": "La création d'une commande nécessite un produit"}), 422
 
-	json_payload = request.json['product']
+	product = Product.get_or_none(json_payload.id)
+	
 	new_order = Order()
 	new_order.product = json_payload
 	new_order.save(force_insert=True)
-	return redirect('/order/' + str(new_order.id))
+	return Response("Location: /order/" + str(new_order.id), 302)
 
 @app.route('/order/<int:order_id>', methods=['GET'])
 def order_get(order_id):
@@ -133,5 +118,5 @@ def order_get(order_id):
 
 @app.cli.command("init-db")
 def init_db():
-    db.create_tables([Product, Transaction, ShippingInformation, Order, CreditCard])
+    db.create_tables([Product, Order])
     get_products()
