654c4c325c61bd96bbd47d6ea53dcfda0e85e278
Philippe Chartrand <dapper2474@gmail.com>
2019-11-02 13:03:02 -0400

    ajout 3 tests

--- test_inf5190.py
+++ test_inf5190.py
@@ -1,7 +1,7 @@
 import pytest
 import json
 import inf5190
-from flask import url_for
+from flask import url_for , jsonify
 
 
 @pytest.fixture
@@ -41,3 +41,28 @@ def test_06_ORDER_POST_MISSING_FIELD(client):
 	data = dict(product=dict(id=1231))
 	status_code = client.post(url_for('order_post'), data=json.dumps(data),headers=headers).status_code
 	assert status_code == 422
+	
+def test_07_ORDER_GET_SUCCESS(client):
+	status_code = client.get(url_for('order_get',order_id=1)).status_code
+	assert status_code == 200
+	
+def test_08_ORDER_PUT_NO_EMAIL(client):
+	headers = {'Content-Type' : 'application/json'}
+	data = dict(order=dict(shipping_information=dict(country='canada',province='QC')))
+	status_code = client.put(url_for('order_put',order_id=1),data=json.dumps(data),headers=headers).status_code
+	assert status_code == 422
+	
+def test_09_ORDER_PUT_SHIPPING_INFO_SUCCESS(client):
+	headers = {'Content-Type' : 'application/json'}
+	data = dict(order=dict(email="caissy.jean-philippe@uqam.ca",shipping_information=dict(country='canada', address='201, rue president kennedy' , postal_code = 'H2X 3Y7' , city = 'Montreal' , province='QC')))
+	response = client.put(url_for('order_put',order_id=1),data=json.dumps(data),headers=headers).status_code
+	assert response == 302
+	
+def test_10_ORDER_PUT_ORDER_NOT_FOUND(client):
+	headers = {'Content-Type' : 'application/json'}
+	data = dict(order=dict(email="caissy.jean-philippe@uqam.ca",shipping_information=dict(country='canada', address='201, rue president kennedy' , postal_code = 'H2X 3Y7' , city = 'Montreal' , province='QC')))
+	response = client.put(url_for('order_put',order_id=999),data=json.dumps(data),headers=headers).status_code
+	assert response == 404
+	
+	
+	
