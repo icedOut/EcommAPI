ab810357105497d1c0815623fda61be521be6993
Philippe Chartrand <dapper2474@gmail.com>
2019-12-17 11:10:09 -0500

    psgql

--- inf5190.py
+++ inf5190.py
@@ -10,10 +10,10 @@ from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 import os
 import psycopg2
-from urllib.parse import urlparse
+from urlparse import urlparse
 
 urlparse.uses_netloc.append('postgres')
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+url = urlparse(os.environ["DATABASE_URL"])
 db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
 db_proxy.initialize(db)
 
