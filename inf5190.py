810cca031e1c15a6a519f341dafe14dc55725098
Philippe Chartrand <dapper2474@gmail.com>
2019-12-17 10:35:11 -0500

    DB postgres

--- inf5190.py
+++ inf5190.py
@@ -9,14 +9,12 @@ from playhouse.shortcuts import model_to_dict, dict_to_model
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 import os
-import psycopg2
+import urlparse, psycopg2
 
-if 'HEROKU' in os.environ:
-    import urlparse, psycopg2
-    urlparse.uses_netloc.append('postgres')
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
-    db_proxy.initialize(db)
+urlparse.uses_netloc.append('postgres')
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+db_proxy.initialize(db)
 
 app = Flask(__name__)
 
