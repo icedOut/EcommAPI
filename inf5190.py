dc3344ac115d8da50c13c7278a4e674202caedf8
Philippe Chartrand <dapper2474@gmail.com>
2019-12-17 20:18:47 -0500

    ajout support BD postgres en mode local

--- inf5190.py
+++ inf5190.py
@@ -13,70 +13,72 @@ import os
 import psycopg2
 from playhouse.db_url import connect
 
-db =  connect(os.environ.get('DATABASE_URL'))
+
+if 'HEROKU' in os.environ:
+	db =  connect(os.environ.get('DATABASE_URL'))
+else:
+	 db = p.PostgresqlDatabase('dak5as5kodulg3', user='jmzhnzsjnbmfqd', password='cd9a7720ce6c5a7a10634b356897c5b1c00660bdc9a21f668c2ccbeb922ef386', host='ec2-174-129-255-106.compute-1.amazonaws.com', port=5432)
 
 app = Flask(__name__)
 
-
-
 class JSONField(p.TextField):
-    """
-    Class to "fake" a JSON field with a text field. Not efficient but works nicely
-    """
-    def db_value(self, value):
-        """Convert the python value for storage in the database."""
-        return value if value is None else json.dumps(value)
+	"""
+	Class to "fake" a JSON field with a text field. Not efficient but works nicely
+	"""
+	def db_value(self, value):
+		"""Convert the python value for storage in the database."""
+		return value if value is None else json.dumps(value)
 
-    def python_value(self, value):
-        """Convert the database value to a pythonic value."""
-        return value if value is None else json.loads(value)
+	def python_value(self, value):
+		"""Convert the database value to a pythonic value."""
+		return value if value is None else json.loads(value)
 
 class BaseModel(p.Model):
-    class Meta:
-        database = db
+	class Meta:
+		database = db
 
 class Product(BaseModel):
-    id=p.IntegerField(primary_key=True)
-    in_stock = p.BooleanField(default=True)
-    description= p.TextField()
-    price = p.DoubleField()
-    image = p.TextField()
-    name = p.TextField()
-    weight = p.IntegerField()
+	id=p.IntegerField(primary_key=True)
+	in_stock = p.BooleanField(default=True)
+	description= p.TextField()
+	price = p.DoubleField()
+	image = p.TextField()
+	name = p.TextField()
+	weight = p.IntegerField()
 
 class Order(BaseModel):
-    id=p.AutoField(primary_key=True)
-    product = JSONField(p.TextField(null=True))
-    credit_card=JSONField(default={})
-    shipping_information=JSONField(default={})
-    transaction= JSONField(default={})
-    shipping_price=p.DoubleField(default=0)
-    email=p.TextField(null=True)
-    paid=p.BooleanField(default=False)
-    total_price=p.DoubleField(default=0)
+	id=p.AutoField(primary_key=True)
+	product = JSONField(p.TextField(null=True))
+	credit_card=JSONField(default={})
+	shipping_information=JSONField(default={})
+	transaction= JSONField(default={})
+	shipping_price=p.DoubleField(default=0)
+	email=p.TextField(null=True)
+	paid=p.BooleanField(default=False)
+	total_price=p.DoubleField(default=0)
 
 
 
 def perform_request(uri, method="GET", data=None):
-    request = Request('https://caissy.dev/shops/{0}'.format(uri))
-    request.method = method
-    request.add_header("content-type", "application/json")
+	request = Request('https://caissy.dev/shops/{0}'.format(uri))
+	request.method = method
+	request.add_header("content-type", "application/json")
 
-    if data:
-        request.data = json.dumps(data).encode('utf-8')
+	if data:
+		request.data = json.dumps(data).encode('utf-8')
 
-    try:
-        with urlopen(request) as response:
-            data = response.read()
-            headers = response.headers
+	try:
+		with urlopen(request) as response:
+			data = response.read()
+			headers = response.headers
 
-            if headers['content-type'] == "application/json":
-                 return json.loads(data)
-            else:
-                return None
-    except HTTPError as e:
-    	data = e.read().decode()
-    	return json.loads(data)
+			if headers['content-type'] == "application/json":
+				 return json.loads(data)
+			else:
+				return None
+	except HTTPError as e:
+		data = e.read().decode()
+		return json.loads(data)
 
 def get_products():
 	json_products = perform_request("products")
@@ -86,12 +88,12 @@ def get_products():
 
 @app.route('/', methods=['GET'])
 def products_get():
-    products = []
+	products = []
 
-    for product in Product.select():
-    	products.append(model_to_dict(product))
+	for product in Product.select():
+		products.append(model_to_dict(product))
 
-    return jsonify(products)
+	return jsonify(products)
 
 @app.route('/order', methods=['POST'])
 def order_post():
@@ -215,8 +217,8 @@ def order_put_credit_card(json_payload, order_id):
 
 @app.cli.command("init-db")
 def init_db():
-    db.create_tables([Product, Order])
-    get_products()
+	db.create_tables([Product, Order])
+	get_products()
  
 def reset_orders():
 	for order in Order().select():
